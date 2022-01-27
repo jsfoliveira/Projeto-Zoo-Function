f268718cf53632602c435dece23a7d07d239426d
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-01-26 21:45:27 -0300

    requisito 5 finalmente finalizado!!!!!!

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,14 +1,24 @@
 const data = require('../data/zoo_data');
+
 // RESOLUÇÃO(parte 1): Se não for passado parâmetro, então retorne um objeto com o element.name e element.residents.length. O forEach vai percorrer cada item do objeto species. Dentro do forEach coloqui objeto[chave] = atribuição e depois retornei (peguei a dica de Luá no Slack).
+// RESOLUÇÃO (parte 2): Vou procurar se o nome adiconado no parâmetro se tem alguma chave igual ao parâmetro, se for true, retorna o value da chave residents.length.
+// RESOLUÇÃO (parte 3): Estou filtrando os residents, em que resident.sex é igual ao animal.sex(o parâmetro que foi colocado). Tive ajuda da minha colega Noelma para entender essas duas últimas partes.
+const { species } = data;
+const obj = {};
 function countAnimals(animal) {
-    const { species } = data;
-    if (animal === undefined) {
-    const obj = {};
+  if (animal === undefined) {
     species.forEach((element) => {
-        obj[element.name] = element.residents.length;
+      obj[element.name] = element.residents.length;
     });
     return obj;
-    }
+  }
+  const animalCount = species
+    .find((specie) => specie.name
+      .includes(animal.specie));
+  if (!animal.sex) {
+    return animalCount.residents.length;
+  }
+  return animalCount.residents.filter((resident) => resident.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
+// quando passado no parâmetro o id do funcionário, vai buscar o employess.responsibleFor e buscar a primeira espécie nesse array. Vai retornar um array com nome, sexo e idade do masi velho no species.residents.
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // const buscar = employees.find((element) => element.id === 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
+  // console.log(buscar);
+
 }
 
 module.exports = getOldestFromFirstSpecies;
